1bbad1499367674975f9a082375b2d0b55613891
tryoma <tryoma0326@gmail.com>
2020-08-01 10:16:27 +0000

    add linebot_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,10 +40,11 @@ class EventsController < ApplicationController
   def notification #LINEアカウント全員に、別途定義したメッセージを送る
     message={       
        type: "text",
-       text: "新しく稽古会が追加されました！確認してみましょう！\nhttps://aqueous-everglades-07337.herokuapp.com/events/#{@event.id.to_s}"
+       text: "新しく稽古会が追加されました！確認してみましょう！\nhttps://aqueous-everglades-07337.herokuapp.com/events/"
      }
     client.broadcast(message)
   end
+  
   def edit
     @event = Event.find(params[:id])
   end
