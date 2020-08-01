90b51ebaf2c77d9879830ccd41b562c524667669
tryoma <tryoma0326@gmail.com>
2020-08-01 02:35:44 +0000

    add linebot_controller

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -11,14 +11,14 @@ class WebhookController < ApplicationController
   end
  
   def callback
-    body = request.body.read
+    body = 'バーカ'
  
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
  
-    events = "バーカ"
+    events = client.parse_events_from(body)
  
     events.each { |event|
       case event
